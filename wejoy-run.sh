89a80fa0120d1d93f83c09dec5695582f7b3f60e
pvillaverde <pvillaverde@qualigy.com>
2022-10-27 23:57:58 +0200

    [Fix] wejoy path scripted

--- wejoy-run.sh
+++ wejoy-run.sh
@@ -1,3 +1,4 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # Game launch script for STEAM
 # set launch options to "/route/to/wejoy-run.sh %command%"
 cleanup() {
@@ -6,7 +7,7 @@ cleanup() {
     pkill -P $$
 }
 # Customize the LUA config script you want to use.
-wejoy /etc/wejoy/config.lua &
+wejoy $SCRIPT_DIR/config.lua &
 WEJOY_PID=$!
 trap cleanup EXIT
 "$@"
